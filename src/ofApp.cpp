47efaf0f7f3436eaa28de2e44e94b9d6e00cea4b
dongzoolee <nant0313@gmail.com>
2021-06-18 17:16:57 +0900

    Create rhythm element

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,78 +1,190 @@
 #include "ofApp.h"
-#include <iostream>
 using namespace std;
+
 //--------------------------------------------------------------
 void ofApp::setup() {
-	//xPos = 5; // horizontal start position
-	//ofBackground(ofColor::black); // black background
+	ofSetFrameRate(15); // Limit the speed of our program to 15 frames per second
+
+	ofBackground(0);
+	consolas30.load("consolas.ttf", 30, true, true);
+	consolas15.load("consolas.ttf", 15, true, true);
+
+	isMainPage = 1;
 }
 
-void ofApp::update() {
-	//xPos += 2;
-	//if (ofGetWidth() < xPos) { // if horizontal position is off the screen (width) 		xPos = 5; // reset horizontal position
-	//}
+//--------------------------------------------------------------
+void ofApp::update() { 
+
 }
 
+//--------------------------------------------------------------
+int x = 30, y = 80;
 void ofApp::draw() {
-	//ofSetColor(ofColor::red); // draw everything in red
-	//ofDrawCircle(xPos, 100, 10); // draw a circle at the (variable) horizontal position, 100 pixels from the top with a 10 pixel diameter
+	ofSetFrameRate(100); // Limit the speed of our program to 15 frames per second
+	ofSetColor(255, 255, 255);
+
+	if (isMainPage) {
+		consolas30.drawString("Rhythm Sonic", ofGetWidth() / 2 - 170, ofGetHeight() / 2);
+		consolas15.drawString("dongzoolee / Sogang University", ofGetWidth() / 2 - 170, ofGetHeight() / 2 + 30);
+		consolas15.drawString("press any key to start", ofGetWidth() / 2 - 170, ofGetHeight() / 2 + 60);
+	}
+	
+	if (isPlayPage) {
+		drawPlayBox();
+		drawRhythmElement(x, y);
+		y += 4;
+	}
+
+	
+}
+
+void ofApp::drawRhythmElement(int x, int y) {
+	roundedRect(x, y, 50, 10, 20);
+}
+
+void ofApp::drawPlayBox() {
+	int width = ofGetWidth();
+	int height = ofGetHeight();
+
+	glm::vec2 playbox_right_top = { width - 30, 80 };
+	glm::vec2 playbox_right_bottom = { width - 30, height - 80 };
+	glm::vec2 playbox_left_top = { 30, 80 };
+	glm::vec2 playbox_left_bottom = { 30, height - 80 };
+
+	ofDrawLine(playbox_left_top, playbox_right_top);
+	ofDrawLine(playbox_right_top, playbox_right_bottom);
+	ofDrawLine(playbox_right_bottom, playbox_left_bottom);
+	ofDrawLine(playbox_left_bottom, playbox_left_top);
+}
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (isMainPage) {
+		isMainPage = 0;
+		isPlayPage = 1;
+	}
+	if (key == 'v') {
+		// HACK: only needed on windows, when using ofSetAutoBackground(false)
+		glReadBuffer(GL_FRONT);
+		ofSaveScreen("savedScreenshot_" + ofGetTimestampString() + ".png");
+	}
+	/*if (key == 'q') {
+		_Exit(0);
+	}*/
+	if (key == 's') {
+	
+	}
+	if (key == 'd') {
+		
+	}
+	if (key == 'f') {
+
+	}
+	if (key == ' ') {
+
+	}
+	if (key == 'j') {
+
+	}
+	if (key == 'k') {
+
+	}
+	if (key == 'l') {
+
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+	/*
+	if (key == 'l') {
+		// Open the Open File Dialog
+		ofFileDialogResult openFileResult = ofSystemLoadDialog("Select a only txt for Waterfall");
+
+		// Check whether the user opened a file
+		if (openFileResult.bSuccess) {
+			ofLogVerbose("User selected a file");
+
+			// We have a file, so let's check it and process it
+			processOpenFileSelection(openFileResult);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
 }
 
 void ofApp::processOpenFileSelection(ofFileDialogResult openFileResult) {
-	cout << openFileResult;
-}
+	//Path to the comma delimited file
+	//string fileName = "input.txt";
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+	string fileName = openFileResult.getName();
+	ofFile file(fileName);
 
-}
+	if (!file.exists()) cout << "Target file does not exists." << endl;
+	else cout << "We found the target file." << endl;
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+	ofBuffer buffer(file);
 
-}
+	/* This variable is for indicating which type of input is being received.
+	 IF input_type == 0, then work of getting line input is in progress.
+	 IF input_type == 1, then work of getting dot input is in progress.
+	 */
+	int input_type = 0;
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
 
-}
+	/* COMSIL1-TODO 1 : Below code is for getting the number of line and dot, getting coordinates.
+	 You must maintain those information. But, currently below code is not complete.
+	 Also, note that all of coordinate should not be out of screen size.x
+	 However, all of coordinate do not always turn out to be the case.
+	 So, You have to develop some error handling code that can detect whether coordinate is out of screen size.
+	*/
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::initializeWaterLines() {
+	;
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,26 +1,73 @@
 #pragma once
+
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+struct line {
+	int x1, y1, x2, y2;
+};
 
-	public:
-		void setup();
-		void update();
-		void draw();
+struct dot {
+	int x, y;
+};
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
-		void ofApp::processOpenFileSelection(ofFileDialogResult openFileResult);
+class ofApp : public ofBaseApp {
 
-		int xPos;
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	// custom functions
+	void drawRhythmElement(int x, int y);
+	void drawPlayBox();
+
+	// custom shapes
+	void roundedRect(float x, float y, float w, float h, float r) {
+		ofBeginShape();
+		ofVertex(x + r, y);
+		ofVertex(x + w - r, y);
+		quadraticBezierVertex(x + w, y, x + w, y + r, x + w - r, y);
+		ofVertex(x + w, y + h - r);
+		quadraticBezierVertex(x + w, y + h, x + w - r, y + h, x + w, y + h - r);
+		ofVertex(x + r, y + h);
+		quadraticBezierVertex(x, y + h, x, y + h - r, x + r, y + h);
+		ofVertex(x, y + r);
+		quadraticBezierVertex(x, y, x + r, y, x, y + r);
+		ofEndShape();
+	}
+	void quadraticBezierVertex(float cpx, float cpy, float x, float y, float prevX, float prevY) {
+		float cp1x = prevX + 2.0 / 3.0 * (cpx - prevX);
+		float cp1y = prevY + 2.0 / 3.0 * (cpy - prevY);
+		float cp2x = cp1x + (x - prevX) / 3.0;
+		float cp2y = cp1y + (y - prevY) / 3.0;
+
+		// finally call cubic Bezier curve function  
+		ofBezierVertex(cp1x, cp1y, cp2x, cp2y, x, y);
+	};
+
+	// font variable
+	ofTrueTypeFont consolas15;
+	ofTrueTypeFont consolas30;
+	// flag variables
+	bool isMainPage = 0;
+	bool isPlayPage = 0;
+	// Line segment and dot related variables
+
+	/* WaterFall-related member functions */
+
+	void processOpenFileSelection(ofFileDialogResult openFileResult);
+	void initializeWaterLines(); // 2nd week portion.
 };
